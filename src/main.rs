96a25af1c4b22bf89c249121a1ec1f468528ecaa
Dillon Fagan <dillonfagan@pm.me>
2020-05-04 16:28:48 -0400

    Update version to 0.1.1

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use mobbit::Turn;
 
 fn main() {
     let matches = App::new("mobbit")
-        .version("0.1.0")
+        .version("0.1.1")
         .about("The cool way to mob.")
         .author("Dillon Fagan")
         .subcommand(
